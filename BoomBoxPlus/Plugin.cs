01b16cdf9d2597d22d9b12f793074041ded3d57f
lundjrl <jrlbidamin@gmail.com>
2024-01-01 11:28:34 -0500

    Saving base-work

---
+++ BoomBoxPlus/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BoomBoxPlus
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BoomBoxBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Lundjrl.BoomBoxPlus";
+        private const string modName = "Boom Box Plus";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static BoomBoxBase Instance;
+
+        internal ManualLogSource mls;
+
+        // Unity onInit function
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The test mod is woke.");
+
+            harmony.PatchAll(typeof(BoomBoxBase));
+        }
+    }
+}
